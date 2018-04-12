4f9dc525ff745241f5fc58263f4e073cba6a0cbb
Correa-Perez <0277355@students.dadeschools.net>
2018-04-12 12:00:39 -0400

    Did a little bit of everything

---
+++ logic.js
@@ -0,0 +1,22 @@
+var panel = $("");
+var countStartNumber = 30;
+
+//Question set 
+var  questions = [ {
+	question: "What was the name of the show that had twins in a hotel?", 
+	answers: ["The Suite Life of Zack and Cody", "Phineas and Ferb", "Lilo and Stitch"],
+	correctAnswer: "The Suite Life of Zack and Cody",
+	image: "assets/MV5BMTk5MTgxMzAzN15BMl5BanBnXkFtZTcwMjUyMTk0MQ@@._V1_UY1200_CR111,0,630,1200_AL_.jpg";
+
+},
+	question: "Who was the little brother of Wizards of Waverly Place?", 
+	answers: ["Max", "Justin", "Maxine"],
+	correctAnswer: "Max",
+	image: "assets/MV5BMTk5MTgxMzAzN15BMl5BanBnXkFtZTcwMjUyMTk0MQ@@._V1_UY1200_CR111,0,630,1200_AL_.jpg";
+
+
+
+
+
+
+]
